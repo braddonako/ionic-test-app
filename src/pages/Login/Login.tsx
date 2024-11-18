95ca97a45a359c42f59d67dd03f4c5acfa5a760e
braddonako <brad@prowalletusa.com>
2024-11-18 10:29:45 -0700

    login page is complete

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,6 +1,7 @@
-import { IonContent, IonPage, IonButton } from '@ionic/react';
+import { IonContent, IonPage, IonButton, IonText } from '@ionic/react';
 import { signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
 import { auth } from '../../firebase/config';
+import './Login.css';
 
 const Login: React.FC = () => {
   const signInWithGoogle = async () => {
@@ -13,13 +14,32 @@ const Login: React.FC = () => {
   };
 
   return (
-    <IonPage>
-      <IonContent>
-        <IonButton expand="block" onClick={signInWithGoogle}>
+<IonPage>
+  <IonContent fullscreen className="ion-padding">
+    <div className="login-container">
+      <div className="login-card">
+        <div className="login-header">
+          <h1>Welcome to</h1>
+          <IonText color="primary" className="app-title">Applitrack</IonText>
+          <p className="subtitle">Track your job applications with ease</p>
+        </div>
+        
+        <IonButton 
+          expand="block" 
+          onClick={signInWithGoogle}
+          className="google-button"
+        >
+          <img 
+            src="/assets/google-logo.png" 
+            alt="Google" 
+            className="google-icon" 
+          />
           Sign in with Google
         </IonButton>
-      </IonContent>
-    </IonPage>
+      </div>
+    </div>
+  </IonContent>
+</IonPage>
   );
 };
 
